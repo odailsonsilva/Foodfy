53253b47158a4c68839516a0749d32bf4099ad49
odailsonsilva <odafreelancerti@gmail.com>
2020-06-15 16:12:39 -0300

    chore: Pg receita  - rota - estrutura - redimencionamento

--- public/scripts.js
+++ public/scripts.js
@@ -1,24 +1,10 @@
 
 const articles = document.querySelectorAll('article')
-const modalOverlay = document.querySelector('.modal-overlay')
-const close = document.querySelector('.modal-close')
 
 
 for(let article of articles){
     article.addEventListener("click", function(){
-        const img = article.getAttribute("id")
-        const title = article.querySelector("p").innerHTML
-        const legend = article.querySelector("legend").innerHTML
-
-        modalOverlay.classList.add('active')
-
-        modalOverlay.querySelector("img").src = `./assets/${img}`
-        modalOverlay.querySelector("h2").innerHTML = title
-        modalOverlay.querySelector("p").innerHTML = legend
-       
+        const receitaID = article.getAttribute("id")
+        window.location.href = `/receita?id=${receitaID}`
     })
 }
-
-close.addEventListener("click", function(){
-    modalOverlay.classList.remove('active')
-})
--- server.js
+++ server.js
@@ -28,5 +28,21 @@ server.get('/receitas', function(req, res){
     return res.render('receitas', {dados})
 })
 
+server.get('/receita', function(req, res){ //QUERY PARM
+    const id = req.query.id //Query Parm (url)
+
+    const receita = dados.find(function(receita){
+        if(receita.id == id){
+            return true
+        }
+    })
+
+    if(!receita){
+        return res.send("Receita not found")
+    }
+
+    res.render("receita", { receita })
+})
+
 // porta para o servidor
 server.listen(5000)
