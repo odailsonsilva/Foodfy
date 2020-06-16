8962b1d86c3cb177cd43326d8accf6d873104226
odailsonsilva <odafreelancerti@gmail.com>
2020-06-16 09:08:42 -0300

    fecture: btn-receita mostrar/esconder

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,11 @@
 
 const articles = document.querySelectorAll('article')
+const btnEsconder = document.querySelectorAll('.btn-esconder')
 
+// funcoes para esconder btn-esconder
+
+
+// funcoes para mandar de receitas -> receita
 
 for(let article of articles){
     article.addEventListener("click", function(){
@@ -8,3 +13,31 @@ for(let article of articles){
         window.location.href = `/receita?id=${receitaID}`
     })
 }
+
+for(let btn of btnEsconder){
+    btn.addEventListener("click", function(){
+        const divAnimacao = document.querySelector(".div-animacao")
+    
+        if(btn.innerHTML == "MOSTRAR" ){
+            btn.innerHTML = "ESCONDER" 
+            divAnimacao.classList.remove("alt")
+        }else if(btn.innerHTML == "ESCONDER"){
+            btn.innerHTML = "MOSTRAR" 
+            divAnimacao.classList.add("alt")
+        }
+    })
+}
+
+
+
+// btnEsconder.addEventListener("click", function(){
+//     const divAnimacao = document.querySelector(".div-animacao")
+
+//     if(btnEsconder.innerHTML == "MOSTRAR" ){
+//         btnEsconder.innerHTML = "ESCONDER" 
+//         divAnimacao.classList.remove("alt")
+//     }else if(btnEsconder.innerHTML == "ESCONDER"){
+//         btnEsconder.innerHTML = "MOSTRAR" 
+//         divAnimacao.classList.add("alt")
+//     }
+// })
